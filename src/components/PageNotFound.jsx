1c249ad3f876cb034180941421bc4d6816798d1c
Aksh-007 <akshaybarapatre54@gmail.com>
2023-10-13 10:22:17 +0530

    Fix - small bug ixes

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 const PageNotFound = () => {
   return (
     <section className="page_404">
-      <div className="container">
+      <div className="four-container">
         <div className="row">
           <div className="col-sm-12 ">
             <div className="col-sm-10 col-sm-offset-1  text-center">
               <div className="four_zero_four_bg">
-                <h1 className="text-center ">404</h1>
+                <h1 className="four-text ">404</h1>
               </div>
 
               <div className="contant_box_404">
